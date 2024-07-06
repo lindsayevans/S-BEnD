6aa64e85648705cd16729308d294a92799255855
Lindsay Evans <lindsay.evans@boomworks.com.au>
2024-07-07 09:28:35 +1000

    refactor: remove unused cruft

--- demo/typings.d.ts
+++ demo/typings.d.ts
@@ -1,7 +1,3 @@
-declare module '*.css' {
-  const classes: Record<string, string>;
-  export default content;
-}
 declare module '*.module.css' {
   const classes: Record<string, string>;
   export default content;
